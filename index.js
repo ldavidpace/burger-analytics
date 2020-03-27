f07ad626508111a07f3df00f6b3862d5300178dc
David Pace <david.pace@domo.com>
2020-03-27 12:07:02 -0600

    fixing the port

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-
-app.listen(3000, () => console.log('Gator app listening on port 3000!'));
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`app listening on port ${port}!`));
