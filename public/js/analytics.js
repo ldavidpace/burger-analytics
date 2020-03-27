d47d209fcfa8311690dadabe61e70be7a52ae1c3
David Pace <david.pace@domo.com>
2020-03-27 14:11:41 -0600

    back to min

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -2,7 +2,7 @@
   var cly = document.createElement('script'); cly.type = 'text/javascript'; 
   cly.async = true;
   // Enter url of script here (see below for other option)
-  cly.src = 'https://cdn.jsdelivr.net/npm/countly-sdk-web@latest/lib/countly.js';
+  cly.src = 'https://cdn.jsdelivr.net/npm/countly-sdk-web@latest/lib/countly.min.js';
   cly.onload = function(){
     Countly.init({
       app_key: "12345",
