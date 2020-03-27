6595358cee85919421cf9268f167258ba498e0aa
David Pace <david.pace@domo.com>
2020-03-27 13:03:37 -0600

    Added analytics to all the html files.

---
+++ public/js/analytics.js
@@ -0,0 +1,8 @@
+(function() {
+  var cly = document.createElement('script'); cly.type = 'text/javascript'; 
+  cly.async = true;
+  // Enter url of script here (see below for other option)
+  cly.src = 'https://cdn.jsdelivr.net/npm/countly-sdk-web@latest/lib/countly.min.js';
+  cly.onload = function(){Countly.init()};
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(cly, s);
+})();
